5785b943d44dce79b89a96fcb96335790fddc724
sharathchandrakarnati <95628854+sharathchandrakarnati@users.noreply.github.com>
2023-04-07 12:45:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,json,jsonify,request
 import pickle
 import numpy as np 
 import requests
+from sklearn.preprocessing import StandardScaler 
 
 
 ## initilazing app 
@@ -14,6 +15,7 @@ def home():
 def predict():
     if request.method == 'POST':
         CRIM=float(request.form['CRIM'])
+        print(CRIM)
         ZN = float(request.form['ZN'])
         INDUS = float(request.form['INDUS'])
         CHAS =float(request.form['CHAS'])
@@ -25,14 +27,18 @@ def predict():
         TAX = float(request.form['TAX'])
         PTRATIO = float(request.form['PTRATIO'])
         B = float(request.form['B'])
+        LSTAT = float(request.form['LSTAT'])
         ##loading pickle file 
         filename = 'ridge_new_model.pickle'
         loaded_model = pickle.load(open(filename,'rb'))
-        data = np.array(([CRIM,ZN,INDUS,CHAS,NOX,RM,AGE,DIS,RAD,TAX,PTRATIO,B]))
-        data.reshape(1,2)
-        
-        my_prediction = loaded_model.predict(data)
-        return render_template('index.html',prediction = my_prediction)
+        data = np.array([CRIM,ZN,INDUS,CHAS,NOX,RM,AGE,DIS,RAD,TAX,PTRATIO,B,LSTAT]).reshape(1,-1)
+        sc = StandardScaler()
+        x = sc.fit_transform(data)
+        my_prediction = loaded_model.predict(x)
+        print(my_prediction)
+        return render_template('index.html',Prediction_text = my_prediction)
 if __name__ =='__main__':
     app.run(debug=True)        
+
+
 
